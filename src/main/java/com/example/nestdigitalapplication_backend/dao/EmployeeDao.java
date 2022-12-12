b526adae9f6aa5007f08b083a60d35d64ebf47e1
sherin200 <sherinsebastian21>
2022-12-12 15:21:28 +0530

    delete and employ login

--- src/main/java/com/example/nestdigitalapplication_backend/dao/EmployeeDao.java
+++ src/main/java/com/example/nestdigitalapplication_backend/dao/EmployeeDao.java
@@ -1,6 +1,8 @@
 package com.example.nestdigitalapplication_backend.dao;
 
 import com.example.nestdigitalapplication_backend.model.Employee;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -9,8 +11,18 @@ import java.util.List;
 
 public interface EmployeeDao extends CrudRepository<Employee,Integer> {
 
-    @Query(value = "SELECT `id`, `address`, `company`, `confirm`, `designation`, `empcode`, `empname`, `mobno`, `password`, `salary`, `username` FROM `employee` WHERE `empcode`=:empcode",nativeQuery = true)
+    @Query(value = "SELECT `id`, `designation`, `empcode`, `empname`, `password`, `salary`, `username` FROM `employees` WHERE `empcode`=:empcode",nativeQuery = true)
     List<Employee> SearchEmployee(@Param("empcode") Integer empcode);
+
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM `employees` WHERE `id`=:id",nativeQuery = true)
+    void DeleteEmployee(@Param("id")Integer id);
+
+    @Query(value = "SELECT `id`, `designation`, `empcode`, `empname`, `password`, `salary`, `username` FROM `employees` WHERE `username`=:username AND `password`=:password",nativeQuery = true)
+    List<Employee> EmployLogin(@Param("username") String username,@Param("password") String password);
+
 
 
 
