d4143f50cd9df86986bc859fcad1e2c397e92df8
sherin200 <sherinsebastian21>
2022-12-12 11:53:13 +0530

    first commit

---
+++ src/main/java/com/example/nestdigitalapplication_backend/model/Employee.java
@@ -0,0 +1,17 @@
+package com.example.nestdigitalapplication_backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Employees")
+
+public class Employee {
+
+    private int id;
+    private String empcode;
+    private String empname;
+    private String
+
+
+}
---
+++ src/test/java/com/example/nestdigitalapplication_backend/NestdigitalapplicationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nestdigitalapplication_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestdigitalapplicationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
