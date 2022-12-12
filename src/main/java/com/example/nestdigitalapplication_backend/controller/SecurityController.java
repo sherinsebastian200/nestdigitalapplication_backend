12aafb57fd013f139499e45711f7515a280a74a9
sherin200 <sherinsebastian21>
2022-12-12 16:10:50 +0530

    security profile and login

--- src/main/java/com/example/nestdigitalapplication_backend/controller/SecurityController.java
+++ src/main/java/com/example/nestdigitalapplication_backend/controller/SecurityController.java
@@ -5,12 +5,10 @@ import com.example.nestdigitalapplication_backend.dao.SecurityDao;
 import com.example.nestdigitalapplication_backend.model.Employee;
 import com.example.nestdigitalapplication_backend.model.Security;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -33,6 +31,53 @@ public class SecurityController {
         map.put("status", "success");
         return map;
     }
+
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/securitylogin" ,consumes = "application/json",produces = "application/json")
+    public Map<String,String>SecurityLogin(@RequestBody Security s)
+    {
+        String username=s.getUsername().toString();
+        String password=s.getPassword().toString();
+        System.out.println(username);
+        System.out.println(password);
+        List<Security> result=(List<Security>) dao.SecurityLogin(s.getUsername(),s.getPassword());
+        HashMap<String,String> map=new HashMap<>();
+        if (result.size()==0)
+        {
+            map.put("status","failed");
+        }
+        else{
+            int id=result.get(0).getId();
+            map.put("empId",String.valueOf(id));
+            map.put("status","success");
+        }
+        return map;
+
+
+
+
+
+    }
+
+    @CrossOrigin(origins = "*")
+    @GetMapping(path = "/viewsecurity")
+    public List<Security> SecurityView()
+    {
+        return (List<Security>) dao.findAll();
+    }
+
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/securityprofile",consumes = "application/json",produces = "application/json")
+    public List<Security> SecViewByID(@RequestBody Security s)
+    {
+        String id=String.valueOf(s.getId());
+        System.out.println(id);
+        return (List<Security>) dao.SecViewByID(s.getId());
+    }
+
+
+
+
 
 
 
